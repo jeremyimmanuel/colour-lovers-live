d84c0f1c7ba5d1e7403229d1dde19f3ae5f674af
jeremyimmanuel <jeremytandjung@icloud.com>
2021-06-23 16:37:05 -0700

    Api: added colourloversapi file

--- src/services/ColourLoversApi/index.js
+++ src/services/ColourLoversApi/index.js
@@ -1,1 +1,16 @@
-const url = 'http://www.colourlovers.com/api'
+
+import axios from 'axios';
+import path from 'path';
+
+const baseURL = 'http://www.colourlovers.com/api'
+
+export const getNewestPalettes = async function () {
+    const params = { format: 'json' }
+    const url = path.join(baseURL, 'palettes', 'new')
+    try {
+        const { data: newPalettes } = await axios.get(url, { params })
+        return { newPalettes, error: null }
+    } catch (error) {
+        return { newPalettes: null, error }
+    }
+}
