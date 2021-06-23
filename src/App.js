9de4d63e201937d0d42ad8ebd60c566aebece987
Jeremy Tandjung <jeremytandjung@icloud.com>
2021-06-22 23:43:25 -0700

    Added grid for palette cards

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import './App.css';
 import ColorCard from './components/ColorCard';
+import { data } from './data';
 
 function App() {
+  let dataList = data.slice(0, 10).map(({ title, userName, dateCreated, imageUrl, numViews, numVotes }) => {
+    return (
+      <ColorCard title={title} author={userName} timestamp={dateCreated} imageUrl={imageUrl} numViews={numViews} numVotes={numVotes} />
+    )
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>
-          <span>ColourLovers. </span>
+      <header className="App-header h-48 flex flex-row justify-start items-center">
+        <h1 className="text-6xl ml-48">
+          <span className="font-thin">ColourLovers. </span>
           <span>Live.</span>
         </h1>
-        <ColorCard title="Anna Frozen Forever" author="romantic_love" timestamp="3:22 PM" />
       </header>
+
+      <div className="App-body flex justify-center">
+        <div className="grid grid-cols-2 gap-4 w-3/4">
+          {dataList}
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/ColorCard/index.js
+++ src/components/ColorCard/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 
 const ColorCard = (props) => {
+    const timestamp = props.timestamp
+    const timestampSimplified = useMemo(() => {
+        const d = new Date(timestamp)
+        d.toLocaleTimeString('en-US', { hour: 'numeric', minute: '2-digit' })
+        return timestamp.slice(0, 10)
+    }, [timestamp])
+
     return (
-        <div className="flex flex-row justify-between w-1/2">
+        <div className="flex flex-row justify-between text-white">
             <div className="flex flex-col items-start">
-                <h2>{props.title}</h2>
-                <caption>by {props.author} at {props.timestamp}</caption>
-                <div className="bg-black bg-opacity-50 text-white text-sm p-2 rounded-sm">3 views 1 vote</div>
+                <h2 className="text-xl">{props.title}</h2>
+                <caption className="text-sm">by {props.author} at {timestampSimplified}</caption>
+                <div className="bg-black bg-opacity-50 text-white text-sm p-2 rounded-sm mt-4">{props.numViews} views {props.numVotes} vote</div>
             </div>
-            <img className="rounded-md" src="http://www.colourlovers.com/paletteImg/9F1F63/D61B5B/F6DC32/F69C1F/67B7A8/Tequila_Sunrise.png" alt="palette"></img>
+            <img className="rounded-md" src={props.imageUrl} alt="palette"></img>
         </div>
     )
 }
